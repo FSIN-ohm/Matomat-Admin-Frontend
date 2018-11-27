0bb1df3478ee5b48dd3266950234cbeeb2bd8634
Tom <ThomasWaadt@web.de>
2018-11-27 20:28:17 +0100

    enabled Routing between sites
    added RouterLinks to all available pages.

--- Admin-Frontend/src/app/app.module.ts
+++ Admin-Frontend/src/app/app.module.ts
@@ -4,7 +4,9 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { RouterModule, Routes} from '@angular/router';
 import { MatSidenavModule, MatToolbarModule, MatButtonModule, MatIconModule, MatListModule } from '@angular/material';
+
 import { MainNavigationComponent } from './main-navigation/main-navigation.component';
 import { LayoutModule } from '@angular/cdk/layout';
 import { LoginPageComponent } from './login-page/login-page.component';
@@ -16,6 +18,18 @@ import { InventoryListComponent } from './inventory-list/inventory-list.componen
 import { TransactionsComponent } from './transactions/transactions.component';
 import { SalesStatisticsComponent } from './sales-statistics/sales-statistics.component';
 
+// internalSiteRoutes declares all Routes that can be visited on our Project-page:
+// NOTE: internalSiteRoutes must be inserted top to bottom and from most specific to least specific.
+const internalSiteRoutes: Routes = [
+  {path: '', component: DashboardComponent},
+  {path: 'acc-management', component: AccountManagementComponent},
+  {path: 'stock', component: InventoryListComponent},
+  {path: 'login', component: LoginPageComponent},
+  {path: 'products', component: ProductsComponent},
+  {path: 'statistics', component: SalesStatisticsComponent},
+  {path: 'settings', component: SettingsComponent},
+  {path: 'transactions', component: TransactionsComponent},
+];
 
 @NgModule({
   declarations: [
@@ -33,6 +47,7 @@ import { SalesStatisticsComponent } from './sales-statistics/sales-statistics.co
   imports: [
     BrowserModule,
     AppRoutingModule,
+    RouterModule.forRoot(internalSiteRoutes),
     BrowserAnimationsModule,
     MatSidenavModule,
     LayoutModule,
