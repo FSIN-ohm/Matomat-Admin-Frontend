60a3141e9592fbfb4db6b4546586942d60fd5ec4
Tom <ThomasWaadt@web.de>
2018-12-11 11:24:57 +0100

    update Sidenav
    added working Links to all important pages to our sidenav.

--- Admin-Frontend/src/app/app-routing.module.ts
+++ Admin-Frontend/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { AccountManagementComponent} from './account-management/account-management.component';
+import { DashboardComponent} from './dashboard/dashboard.component';
 import { InventoryListComponent } from './inventory-list/inventory-list.component';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { ProductsComponent} from './products/products.component';
@@ -10,11 +11,11 @@ import { SettingsComponent} from './settings/settings.component';
 import { TransactionsComponent} from './transactions/transactions.component';
 import { UserPageComponent } from './user-page/user-page.component';
 
-
 // internalSiteRoutes declares all Routes that can be visited on our Project-page:
 // NOTE: internalSiteRoutes must be inserted top to bottom and from most specific to least specific.
 const internalSiteRoutes: Routes = [
   {path: 'acc-management', component: AccountManagementComponent},
+  {path: 'dashboard', component: DashboardComponent},
   {path: 'stock', component: InventoryListComponent},
   {path: 'login', component: LoginPageComponent},
   {path: 'products', component: ProductsComponent},
